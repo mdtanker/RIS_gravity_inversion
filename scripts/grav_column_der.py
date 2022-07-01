9b07ee66c68173bc26b34a479b20959eee10a19a
mdtanker <matt.d.tankersley@gmail.com>
2022-07-01 17:53:16 +1200

    moved anomaly calc outside of inv funct, added vd.Trend

--- scripts/grav_column_der.py
+++ scripts/grav_column_der.py
@@ -1,21 +1,21 @@
-from scipy import *
-from numpy import *
+import scipy as sp
+import numpy as np
 
 '''
 from the Hammer prism approximation with annulus, as shown on pg. 31, eq:2.19, of Jack McCubbine's thesis
 '''
 
 def grav_column_der(x0,y0,z0,xc,yc,z1,z2,res,rho):
-    r=sqrt((x0-xc)**2+(y0-yc)**2)
+    r=np.sqrt((x0-xc)**2+(y0-yc)**2)
     # r1=r - sqrt(res/2)
     # r2=r + sqrt(res/2) 
     r1=r-0.5*res # fabio's original
     r2=r+0.5*res # fabio's original   
     r1[r1<0]=0 # will fail if prism is under obs point
     r2[r1<0]=0.5*res
-    f=res**2/(pi*(r2**2-r1**2)) #eq 2.19 in McCubbine 2016 Thesis
+    f=res**2/(np.pi*(r2**2-r1**2)) #eq 2.19 in McCubbine 2016 Thesis
     # anomaly_grad=0.0419*f*rho*(z1-z0)*(1/sqrt(r1**2+(z1-z0)**2)-1/sqrt(r2**2+(z1-z0)**2))  # switched r1 and r2  
-    anomaly_grad=0.0419*f*rho*(z1-z0)*(1/sqrt(r2**2+(z1-z0)**2)-1/sqrt(r1**2+(z1-z0)**2)) # fabio's original
+    anomaly_grad=0.0419*f*rho*(z1-z0)*(1/np.sqrt(r2**2+(z1-z0)**2)-1/np.sqrt(r1**2+(z1-z0)**2)) # fabio's original
 
 #    anomaly_grad=0.0419*f*rho*(z2-z0)*(1/sqrt(r1**2+(z2-z0)**2)-1/sqrt(r2**2+(z2-z0)**2))   
     return anomaly_grad    
