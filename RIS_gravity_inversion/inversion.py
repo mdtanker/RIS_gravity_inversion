5bb1ce3b4334906a39b3729fd184ab37cd618724
mdtanker <matt.d.tankersley@gmail.com>
2022-11-24 14:07:58 +1300

    added timing decorator

--- RIS_gravity_inversion/inversion.py
+++ RIS_gravity_inversion/inversion.py
@@ -8,10 +8,24 @@ import verde as vd
 import xarray as xr
 from antarctic_plots import fetch, maps, profile, utils
 from scipy.sparse.linalg import lsqr
+import copy
+import functools
+import time
 
 warnings.filterwarnings("ignore", message="pandas.Int64Index")
 warnings.filterwarnings("ignore", message="pandas.Float64Index")
 
+
+def timer(func):
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        tic = time.perf_counter()
+        value = func(*args, **kwargs)
+        toc = time.perf_counter()
+        elapsed_time = toc - tic
+        print(f"Elapsed time: {elapsed_time:0.4f} seconds")
+        return value
+    return wrapper_timer
 
 def import_layers(
     layers_list: list,
