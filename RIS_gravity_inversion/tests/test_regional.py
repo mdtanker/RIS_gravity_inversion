88492c4d0c35f0f1846e967b8730bc596e884fc5
mdtanker <matt.d.tankersley@gmail.com>
2023-03-13 13:40:08 +1300

    added tests

---
+++ RIS_gravity_inversion/tests/test_regional.py
@@ -0,0 +1,116 @@
+#%%
+import pytest
+
+from RIS_gravity_inversion import regional
+import xarray as xr
+import numpy as np
+import pandas as pd
+import verde as vd
+
+
+def dummy_grid():
+    (x, y, z) = vd.grid_coordinates(
+        region=[-100, 100, 200, 400],
+        spacing=100,
+        extra_coords=20,
+    )
+
+    # create topographic features
+    misfit = y**2
+
+    grid = vd.make_xarray_grid(
+        (x, y),
+        (misfit, z),
+        data_names=("misfit", "upward"),
+        dims=("northing", "easting"),
+    )
+
+    return grid
+
+
+def dummy_df():
+    df = dummy_grid().to_dataframe().reset_index()
+    df["grav"] = 20000
+    return df
+
+
+def test_regional_trend():
+    """
+    test the regional_trend function
+    """
+    anomalies = dummy_df()
+
+    df = regional.regional_trend(
+        trend=0,
+        misfit_grid = dummy_grid().rename({"easting":"x", "northing":"y"}).misfit,
+        anomalies = anomalies,
+        crs = "3031",
+    )
+    print(df)
+    # test  whether regional field has been remove correctly
+    # by whether the means of the reg and misfit are similar
+    assert np.mean(df.reg) == pytest.approx(np.mean(df.misfit), rel=1e-10)
+
+
+def test_regional_filter():
+    """
+    test the regional_filter function
+    """
+    anomalies = dummy_df()
+
+    df = regional.regional_filter(
+        filter_width="g300",
+        misfit_grid = dummy_grid().rename({"easting":"x", "northing":"y"}).misfit,
+        anomalies = anomalies,
+        registration = "g",
+    )
+
+    reg_range = np.max(df.reg) - np.min(df.reg)
+    misfit_range = np.max(df.misfit) - np.min(df.misfit)
+
+    # test  whether regional field has been remove correctly
+    # by whether the limits of the regional are smaller than the limits of the gravity
+    # with a 10% margin
+    assert reg_range < misfit_range*0.9
+
+
+def test_regional_constraints():
+    """
+    test the regional_constraints function
+    """
+    anomalies = dummy_df()
+    points = pd.DataFrame({"easting":[-50, -30, 0, 10, 50], "northing":[210, 280, 240, 360, 310]})
+    df = regional.regional_constraints(
+        constraint_points = points,
+        misfit_grid = dummy_grid().rename({"easting":"x", "northing":"y"}).misfit,
+        anomalies = anomalies,
+        region=[-100, 100, 200, 400],
+        spacing=100,
+        grid_method="verde",
+    )
+
+    reg_range = np.max(df.reg) - np.min(df.reg)
+    misfit_range = np.max(df.misfit) - np.min(df.misfit)
+    # test  whether regional field has been remove correctly
+    # by whether the regional values are close the the grav values at the constraints
+    assert reg_range < misfit_range*0.9
+
+
+def test_regional_eq_sources():
+    """
+    test the regional_eq_sources function
+    """
+    anomalies = dummy_df()
+    anomalies['misfit'] = np.random.normal(100, 100, len(anomalies))
+
+    df = regional.regional_eq_sources(
+        source_depth = 1000e3,
+        anomalies = anomalies,
+    )
+    print(df)
+    reg_range = np.max(df.reg) - np.min(df.reg)
+    misfit_range = np.max(df.misfit) - np.min(df.misfit)
+    print(reg_range, misfit_range)
+    # test  whether regional field has been remove correctly
+    # by whether the regional values are close the the grav values at the constraints
+    assert reg_range < misfit_range*0.9
--- RIS_gravity_inversion/tests/test_utils.py
+++ RIS_gravity_inversion/tests/test_utils.py
@@ -1,2 +1,67 @@
-from RIS_gravity_inversion.utils import utils
+# %%
+import pandas as pd
 import pytest
+import xarray as xr
+import numpy as np
+from RIS_gravity_inversion import utils
+import verde as vd
+import numpy as np
+
+def test_RMSE():
+    """
+    test the RMSE function
+    """
+    # create some dummy data
+    data = np.array([1, 2, 3])
+    # calculate the RMSE
+    rmse = utils.RMSE(data)
+    # test that the RMSE is 0
+    assert rmse == 2.0
+
+
+def test_dist_nearest_points():
+    """
+    test the dist_nearest_points function
+    """
+    # create some dummy data
+    targets = pd.DataFrame({"x": [0, 2], "y": [0, -1]})
+
+    df = pd.DataFrame(
+        {"x": [-4, 4, 0, 4, -4], "y": [-4, 4, 0, -4, 4], "z": [0, 1, 2, 3, 4]}
+    )
+
+    da = df.set_index(["y", "x"]).to_xarray().z
+    ds = df.set_index(["y", "x"]).to_xarray()
+
+    # calculate the distance with a df
+    dist_df = utils.dist_nearest_points(
+        targets,
+        df,
+        coord_names=["x", "y"],
+    )
+
+    # calculate the distance with a da
+    dist_da = utils.dist_nearest_points(
+        targets,
+        da,
+        coord_names=["x", "y"],
+    )
+
+    # calculate the distance with a ds
+    dist_ds = utils.dist_nearest_points(
+        targets,
+        ds,
+        coord_names=["x", "y"],
+    )
+
+    ds_results = np.array(vd.grid_to_table(dist_ds).min_dist)
+    da_results = np.array(vd.grid_to_table(dist_da).min_dist)
+    df_results = np.array(dist_df.min_dist)
+
+    # test that the results all match
+    np.array_equal(ds_results, da_results)
+    np.array_equal(da_results, df_results)
+
+    #test that smallest min_dist and largest min_dist are correct
+    assert np.min(df_results) == pytest.approx(0)
+    assert np.max(df_results) == pytest.approx(5.656854)
